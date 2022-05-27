5bf85f9d4e61a1008409fedb9f25689589a3dac9
Christopher Callender <christophercallender0000@gmail.com>
2022-05-27 18:09:54 -0400

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
 function NavBar() {
-  return (
-    <nav>
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-    </nav>
-  );
+   return (
+      <nav>
+         <a href="#home">Home</a>
+         <a href="#about">About</a>
+      </nav>
+   );
 }
 
 export default NavBar;
